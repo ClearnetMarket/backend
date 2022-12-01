c56d40bf6965b59bba1274897ee0f35d49acd1b7
Anekdotin <eddwinn@gmail.com>
2022-12-01 12:30:51 -0500

    Updated password

--- local_settings.py
+++ local_settings.py
@@ -10,7 +10,7 @@ class ApplicationConfig:
     CURRENT_SETTINGS = 'LOCAL'
     # databases info
     POSTGRES_USERNAME = 'postgres'
-    POSTGRES_PW = 'password'
+    POSTGRES_PW = 'postgres'
     POSTGRES_SERVER = 'database:5432'
     POSTGRES_DBNAME00 = 'clearnet'
     SQLALCHEMY_DATABASE_URI_0 = "postgresql+psycopg2://{}:{}@{}/{}".format(POSTGRES_USERNAME,
